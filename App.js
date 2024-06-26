d8cb663016dc9eee23a9552a817f3fc9e6e0c72a
Gurram, Srinivasa Rao <SrinivasaRao.Gurram@cdk.com>
2024-06-26 10:21:16 +0530

    lesson 3

--- App.js
+++ App.js
@@ -1,28 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-const parent = React.createElement("div",
-    { id: "parent" },
-    [
-        React.createElement("div",
-            { id: "child1" },
-            [
-                React.createElement("h1", {}, "This is Namaste React "),
-                React.createElement("h2", {}, "I am an h2 tag")
-            ]),
-        React.createElement("div",
-            { id: "child2" },
-            [
-                React.createElement("h1", {}, "I am an h1 tag"),
-                React.createElement("h2", {}, "I am an h2 tag")
-            ])
-    ]);
-
+// React.createElement => Object => HTMLElement(render)
 
 // const heading = React.createElement("h1",
 //     { id: "heading" },
-//     "Hello World from React!");
+//     "Namaste React"
+// );
+
+// JSX (transpiled before it reaches to JS) - PARCEL usel Bebel to transpile (convert jsx to js which is understood by browser)
+// const heading = (
+//     <h1 className="head" tabIndex="1">
+//         Namaste React using JSX
+//     </h1>
+// );
+
+// JSX => React.createElement => ReactElement - JS Object => HTMLElement(render) 
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+//root.render(heading);
+
+//React Component
+// Class Based Component - OLD
+// Functional Component - NEW
+
+const title = (
+    <h1 className="heading" tabIndex="5">
+        Namaste React Using JSX
+    </h1>
+);
+
+//Component Composition
+const HeadingComponent = () => (
+    <div id="container">
+        {title}
+        <h1 className="heading">Namaste React Functional Component</h1>
+    </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
+root.render(<HeadingComponent />);
